a3fb9bc1337fce784485e61216631318153debdd
Programador Novato <info@programadornovato.com>
2019-06-11 17:55:01 -0500

    06.- Codeigniter 4🔥 [vistas👶]
    https://youtu.be/BzCj2KASr10

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,8 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$estructura=view('estructura/header').view('estructura/body');
+		return $estructura;
 	}
 
 	//--------------------------------------------------------------------
---
+++ app/Views/estructura/body.php
@@ -0,0 +1,4 @@
+<body>
+    Hola mundo desde programadornovato.com
+</body>
+</html>
