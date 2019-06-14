4f36fe410e65ef091efbf554471bd8148fdf810d
Programador Novato <info@programadornovato.com>
2019-06-14 17:59:12 -0500

    17.- Codeigniter 4🔥 [bootstrap]
    https://youtu.be/Bp3_soFGnsg

--- app/Views/estructura/body.php
+++ app/Views/estructura/body.php
@@ -1,21 +1,35 @@
 <body>
-    <table>
-    <tr>
-    <th>id</th>
-    <th>name</th>
-    <th>email</th>
-    <th>deleted</th>
-    </tr>
-    <?php
-    foreach($users as $user){
-        echo "<tr>";
-        echo "<td>".$user['id']."</td>";
-        echo "<td>".$user['name']."</td>";
-        echo "<td>".$user['email']."</td>";
-        echo "<td>".$user['deleted']."</td>";
-        echo "</tr>";
-    }
-    ?>
-    </table>
+<div class="container">
+    <div class="row">
+        <a href="<?php echo base_url(); ?>home/formulario" class="btn btn-info" role="button" >Nuevo</a>
+    </div>
+    <div class="row">
+
+    
+        <table  class="table">
+        <tr>
+        <th scope="col">id</th>
+        <th scope="col">name</th>
+        <th scope="col">email</th>
+        <th scope="col">deleted</th>
+        <th scope="col">acciones</th>
+        </tr>
+        <?php
+        foreach($users as $user){
+            echo "<tr  scope='row' >";
+            echo "<td>".$user['id']."</td>";
+            echo "<td>".$user['name']."</td>";
+            echo "<td>".$user['email']."</td>";
+            echo "<td>".$user['deleted']."</td>";
+            echo "<td>";
+            echo form_button(array('name'=>'editar','type'=>'submit','class'=>'btn btn-warning','content'=>'<i class="fa fa-pencil-square-o"></i>'));
+            echo form_button(array('name'=>'borrar','type'=>'submit','class'=>'btn btn-danger','content'=>'<i class="fa fa-trash"></i>'));
+            echo "</td>";
+            echo "</tr>";
+        }
+        ?>
+        </table>
+    </div>
+</div>
 </body>
 </html>
--- app/Views/estructura/formulario.php
+++ app/Views/estructura/formulario.php
@@ -1,12 +1,20 @@
+<div class="container">
 <?php
 echo form_open('/home/guarda');
+?>
+<div class="form-group">
+<?php
 echo form_label('Nombre','name');
-echo form_input(array('name'=>'name','placeholder'=>'Nombre'));
+echo form_input(array('name'=>'name','placeholder'=>'Nombre','class'=>'form-control'));
 echo "<br>";
 echo form_label('Email','email');
-echo form_input(array('name'=>'email','placeholder'=>'Email'));
+echo form_input(array('name'=>'email','placeholder'=>'Email','class'=>'form-control'));
 echo "<br>";
-echo form_submit('guarda','Guardar');
+echo form_submit('guarda','Guardar','class="btn btn-primary"');
+?>
+</div>
+<?php
 
 echo form_close();
 ?>
+</div>
