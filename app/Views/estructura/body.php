1137aa00c6b9825813eaa9e1e8c0817bb66c5260
Programador Novato <info@programadornovato.com>
2019-06-17 01:38:51 -0500

    Codeigniter 4🔥 [24.- sintaxis alternativa]
    https://youtu.be/0fz68p2HpEw

--- app/Views/estructura/body.php
+++ app/Views/estructura/body.php
@@ -1,7 +1,8 @@
 <?php
-echo $cabecera;
+//echo $cabecera;
 //echo view('estructura/header');
 ?>
+<?= $cabecera; ?>
 <body>
 <div class="container">
     <div class="row">
@@ -19,21 +20,20 @@ echo $cabecera;
         <th scope="col">acciones</th>
         </tr>
         <?php
-        foreach($users as $user){
-            echo "<tr  scope='row' >";
-            echo "<td>".$user['id']."</td>";
-            echo "<td>".$user['name']."</td>";
-            echo "<td>".$user['email']."</td>";
-            echo "<td>".$user['deleted']."</td>";
-            echo "<td>";
-            ?>
+        ?>
+        <? if($users): ?>
+            <?php foreach ($users as $user): ?>
+            <?= "<tr  scope='row' >";?>
+            <?= "<td>".$user['id']."</td>";?>
+            <?= "<td>".$user['name']."</td>";?>
+            <?= "<td>".$user['email']."</td>";?>
+            <?= "<td>".$user['deleted']."</td>";?>
+            <td>
             <a href="<?php echo base_url(); ?>micontrolador/editar/<?php echo $user['id']; ?>" class="btn btn-warning" role="button" ><i class="fa fa-pencil-square-o"></i></a>
             <a href="<?php echo base_url(); ?>micontrolador/borrar/<?php echo $user['id']; ?>" class="btn btn-danger" role="button" ><i class="fa fa-trash"></i></a>
-            <?php
-            echo "</td>";
-            echo "</tr>";
-        }
-        ?>
+            </td>
+            <?php endforeach; ?>
+        <? endif; ?>
         </table>
     </div>
 </div>
